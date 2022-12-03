a3a04a4ebf6de115224eb534e8c6ebc765223e97
SERHII MELNYK <melser68@i.ua>
2022-12-03 13:28:08 +0100

    11

--- Rekursia.py
+++ Rekursia.py
@@ -1,9 +1,11 @@
 a = int(input('Введіть число до якого вирахувати факторіал: '))
 
+
 def factorial(a):
-    if a <=1:
+    if a <= 1:
         return 1
     else:
         return a * factorial(a-1)
+
 
 print('Факторіал числа ' + str(a) + '! = ' + str(factorial(a)))
--- Spiski.py
+++ Spiski.py
@@ -1,5 +1,5 @@
-my_list = ['as', 'hh',2,7,8,8,8,9,9,0]
-z=0
+my_list = ['as', 'hh', 2, 7, 8, 8, 8, 9, 9, 0]
+z = 0
 while z < len(my_list):
     print(my_list[z])
     z = z+1
@@ -7,7 +7,7 @@ my_list.clear()
 print(my_list)
 my_list.insert(0, 'test')
 my_list.append('222')
-my_list.insert(0,'prime')
+my_list.insert(0, 'prime')
 print(my_list)
 ind = my_list.index('222')
 my_list.sort()
