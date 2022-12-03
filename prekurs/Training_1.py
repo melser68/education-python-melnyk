3551de6e274b11bc3a857510c367426900b79e30
SERHII MELNYK <melser68@i.ua>
2022-12-03 12:18:56 +0100

    Update Training_1.py

--- prekurs/Training_1.py
+++ prekurs/Training_1.py
@@ -1,12 +1,11 @@
-arg = input('1_Введіть число або "exit" для виходу: ')    
+arg = input('1_Введіть число або "exit" для виходу: ')
 
-while  arg != 'exit':
+while arg != 'exit':
     try:
         rez = 2 / float(arg)
         print(rez)
-        arg = input('2_Введіть число або "exit" для виходу: ')        
+        arg = input('2_Введіть число або "exit" для виходу: ')
 
     except:
         print('Неприпустимий вид даних.')
-        arg = input('3_Введітььь число або "exit" для виходу: ')
-        
+        arg = input('3_Введіть число або "exit" для виходу: ')
