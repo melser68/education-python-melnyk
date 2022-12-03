bb9922f406598567bf70463e0972dac48928c705
SERHII MELNYK <melser68@i.ua>
2022-12-03 11:50:56 +0100

    Навчання

--- prekurs/Training.py
+++ prekurs/Training.py
@@ -1,6 +1,10 @@
-from pathlib import Path
-
-p = Path('C:\Program Files')
-
-for i in p.iterdir():
-    print(p.exists)
+print('Сервіс ідентифікації для оформлення оренди автомобіля')
+age = int(input('Ваш вік: '))
+name = input("Ваше ім'я: ")
+driver_license = input('Чи маєте Ви водійське посвідчення ? (так, ні) ')
+if age >= 18 and driver_license == 'так':
+    print(f'{name} Ви можете отримати авто !')
+elif driver_license == 'ні':
+    print(f'{name} у Вас немає права на керування, у оренді відмовлено')
+else:
+    print(f'{name} у Вас замалий вік, у оренді відмовлено')
---
+++ prekurs/Training_1.py
@@ -0,0 +1,12 @@
+arg = input('1_Введіть число або "exit" для виходу: ')    
+
+while  arg != 'exit':
+    try:
+        rez = 2 / float(arg)
+        print(rez)
+        arg = input('2_Введіть число або "exit" для виходу: ')        
+
+    except:
+        print('Неприпустимий вид даних.')
+        arg = input('3_Введіть число або "exit" для виходу: ')
+        
